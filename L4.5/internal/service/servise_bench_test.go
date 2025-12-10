c72c38a97c51f570026cea52a44f231c1e9664d2
Vladimirmoscow84 <ccr1@yandex.ru>
2025-12-10 12:56:47 +0300

    fix benchmark

--- L4.5/internal/service/servise_bench_test.go
+++ L4.5/internal/service/servise_bench_test.go
@@ -6,7 +6,7 @@ import (
 	"L4.5/internal/model"
 )
 
-func BenchGetStatsSmall(b *testing.B) {
+func BenchmarkGetStatsSmall(b *testing.B) {
 	nums := model.Numbers{Data: []float64{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}}
 	stat := New()
 	b.ResetTimer()
@@ -15,7 +15,7 @@ func BenchGetStatsSmall(b *testing.B) {
 	}
 }
 
-func BenchGetStatsBig(b *testing.B) {
+func BenchmarkGetStatsBig(b *testing.B) {
 	data := make([]float64, 250000)
 	for i := range data {
 		data[i] = float64(i%100) + float64(i/23)
